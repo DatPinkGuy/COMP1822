c510a8c083d8b7507880b1eb1f916ae6e8f6a69f
Alex <ovcinikov@gmail.com>
2020-02-17 17:39:18 +0000

    Added background for xray camera

--- CW2/Assets/Scripts/XrayCamera.cs
+++ CW2/Assets/Scripts/XrayCamera.cs
@@ -9,7 +9,9 @@ public class XrayCamera : MonoBehaviour
     [SerializeField] private OVRGrabber handGrabber;
     [SerializeField] private Transform snapPoint => GetComponentInChildren<Transform>();
     private Rigidbody rb => GetComponent<Rigidbody>();
-    public bool _handSnap;
+    public bool handSnap;
+    private Vector3 _oldDirection;
+    private float _angle;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +21,7 @@ public class XrayCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!_handSnap) return;
+        if (!handSnap) return;
         CameraFunctionality();
     }
 
@@ -31,7 +33,7 @@ public class XrayCamera : MonoBehaviour
             {
                 if(OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
                 {
-                    _handSnap = true;
+                    handSnap = true;
                 }
             }
         }
@@ -40,11 +42,13 @@ public class XrayCamera : MonoBehaviour
     private void CameraFunctionality()
     {
         handGrabber.transform.position = snapPoint.position;
-        Vector3 rotation = new Vector3(0,handGrabber.transform.rotation.y*-50,0);
+        Vector3 rotation = new Vector3(0,handGrabber.transform.rotation.y*-360,0);
         transform.rotation = Quaternion.Euler(rotation);
+        _oldDirection = handGrabber.transform.InverseTransformDirection(handGrabber.transform.up);
+        xrayCamera.fieldOfView = handGrabber.transform.rotation.y * -90;
         if (!OVRInput.Get(OVRInput.Button.SecondaryHandTrigger))
         {
-            _handSnap = false;
+            handSnap = false;
             //handGrabber.transform.position = new Vector3(0,0,0);
         }
     }
