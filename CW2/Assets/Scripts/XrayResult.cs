188616c1923731558765ca972702de301d3b7afd
Aleksandr <ovcinikov@gmail.com>
2020-03-02 18:35:07 +0000

    Removed unnecessary code
    Removed code that i forgot to remove in last commit

--- CW2/Assets/Scripts/XrayResult.cs
+++ CW2/Assets/Scripts/XrayResult.cs
@@ -17,34 +17,13 @@ public class XrayResult : MonoBehaviour
         _xRayCamera = FindObjectOfType<XrayCamera>();
         _resultArea = FindObjectOfType<ResultArea>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     private void OnTriggerEnter(Collider other)
     {
         var fov = _xRayCamera.xrayCamera.fieldOfView;
-        Debug.Log(fov);
-        Debug.Log(fov > minMaxFov.x && fov < minMaxFov.y);
-        Debug.Log(_resultArea.colliderInArea.Count >= 2);
         if (fov > minMaxFov.x && fov < minMaxFov.y)
         {
             if (_resultArea.colliderInArea.Count >= 2) _isResultFine = true;
         }
-        Debug.Log(_isResultFine);
-        
-        RenderTexture activeRenderTexture = RenderTexture.active;
-        RenderTexture.active = _xRayCamera.xrayCamera.targetTexture;
- 
-        _xRayCamera.xrayCamera.Render();
-
-        image.texture = activeRenderTexture;
-//        Texture2D image = new Texture2D(_xRayCamera.xrayCamera.targetTexture.width,_xRayCamera.xrayCamera.targetTexture.height);
-//        image.ReadPixels(new Rect(0, 0, _xRayCamera.xrayCamera.targetTexture.width, _xRayCamera.xrayCamera.targetTexture.height), 0, 0);
-//        image.Apply();
-//        RenderTexture.active = activeRenderTexture;
     }
 }
