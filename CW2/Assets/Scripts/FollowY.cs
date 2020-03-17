7ab047c3b6f7f725b50d84949baa793036811be3
Aleksandr <ovcinikov@gmail.com>
2020-03-17 15:11:55 +0000

    Xray part follows main xray, moved tables

---
+++ CW2/Assets/Scripts/FollowY.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowY : MonoBehaviour
+{
+    private float _thisY => transform.position.y;
+    [SerializeField] private GameObject xRay;
+    private float XrayY => xRay.transform.position.y;
+    private float _difference;
+
+    private Vector3 _thisPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _difference = XrayY - _thisY;
+    }
+
+    void Update()
+    {
+        ChangeY();
+    }
+
+    private void ChangeY()
+    {
+        _thisPosition = transform.position;
+        _thisPosition.y = xRay.transform.position.y - _difference;
+        transform.position = _thisPosition;
+    }
+}
